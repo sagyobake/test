a5d506e7ee510c0e2fe0de997521a3c0937a8a83
sagyobake <darkmatter8128@gmail.com>
2025-01-11 20:58:16 +0900

    upgrade

---
+++ app.js
@@ -0,0 +1,28 @@
+let user_list = [];
+
+Deno.serve({
+    port: 443, //ポート番号を変更
+    handler: async (request) => {
+        const { socket, response } = Deno.upgradeWebSocket(request);
+
+        const sendToAllClient = value => {
+            user_list.forEach(() => {
+                socket.send(`${value}人目のユーザーが参加しました！`);
+            });
+        }
+
+        socket.onopen = () => {
+            user_list.push(socket);
+            const n = user_list.length;
+            sendToAllClient(n);
+        };
+        socket.onmessage = (e) => {
+            console.log(`RECEIVED: ${e.data}`);
+        };
+        socket.onclose = () => {
+        };
+        socket.onerror = (error) => console.error("ERROR:", error);
+
+        return response;
+    },
+});
